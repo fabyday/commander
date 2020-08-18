e185406727d9b94918b83169bcca257cdebacadb
ROH <rohjihyun95@gmail.com>
2020-08-18 16:12:26 +0900

    add DirectoryEnvironment Class in env.py add StrongTypeChecker Class in typechecker.py
    need to test DE Class and STC... as faster as Possible.

--- env.py
+++ env.py
@@ -60,7 +60,7 @@ class DirectoryManager(object) :
             self.target_name_regex = target_name_regex
 
 
-    def set_path_circuit_style(priorities=[0], sort_priorities=COMMON_NAME):
+    def set_path_circuit_style(self, priorities=[0], sort_priorities=COMMON_NAME):
         """
             FUNCTION set_path_circuit_style
             _______________________________
@@ -188,7 +188,40 @@ class DirectoryManager(object) :
         return return_list
 
 class DirectoryEnvironment(object):
-    def __init__(self, target_dir, working_directory="./"):
+    """
+        Class DirectoryEnvironment : 
+            Usage : 
+                input_obj1 = DirectoryEnvironment(target_path= ..., copy_option_from_reference=None).omit_depth(...).set_target_object(...).compile()
+                                            or 
+                output_obj2 = DirectoryEnvironment(target_path= ..., copy_option_from_reference={obj1} ).compile()
+
+
+    """
+    def __initialize_vars():
+        self.working_directory = None
+        self.target_path = None #if Change this you can easily make output path
+        self.recompile_flag = False
+
+        self.omit_desired_depth = None
+        self.search_depth = None
+        self.sort_option = DESCENDING
+
+        self.raw_data_pathes = None
+        self.copy_detected = False
+
+
+        self.copy_obj = None 
+        
+    def __init__(self, target_path, working_directory="./", copy_option_from_reference=None):
+        """
+            Method __init__ : 
+            - args 
+                (str) target_dir : absolute path or reletive path. if it is reletive path, current dir is depend on working_directory argument.
+                (str) working_directory : set current directory path.
+                (DirectoryEnvironment) copy_refrence : refer to 
+
+        """
+
         if os.path.exists(working_directory) : 
             self.working_directory = os.path.abspath(working_directory)
 
@@ -197,13 +230,150 @@ class DirectoryEnvironment(object):
             self.target_path = target_path
         else : 
             raise EnvironmentError("the abs path for base path is not exists")
+
+
+        if copy_option_from_reference != None : 
+            self.copy_detected = True
+            self.copy_obj = copy_option_from_reference
+
+            #TODO 
+            # self.omit_depth(copy_option_from_reference.desired_depth)
+            #     .set_target_object(copy_option_from_reference.target_name_regex)
+            #     .set_options(copy_option_from_reference.sort_option)
+            
+        
+    def omit_depth(self, desired_depth=None):
+        """
+            Method omit_depth : 
+                -Args : 
+                    (list of Integer & None ) desired_depth : it will start with 0, depth that you will omit. 0 means that target_path.
+                                                    (ex. if exists two pathes. {path/to/temp1/some} , {path/to/temp2/some} -> omit_depth(2) -> {path/to/some} , {path/to/some})
+                - Return
+                    DirectoryEnvironment : self instance
+                    
+
+        """
+        #TODO type checker list and Integer and None
+
+        self.omit_desired_depth = desired_depth
+        return self
+
+    def set_target_object(self, target_name_regex):
+        self.target_name_regex = target_name_regex
+        return self
+    
+    #정렬이 중요하지.. 파일의 길이를 일정하게 만들어주는 방법.
+    def set_options(self, search_depth = -1, sort_option=DESCENDING):
+        if sort_option in [ASCENDING, DESCENDING, COMMON_NAME] : 
+            self.sort_option = sort_option
+        
+        
+        self.search_depth = search_depth
+         
+
+    
+    def compile(self):
+    # def compile(self, recompile_flag = False):
+        """
+            Method compile :
+            
+            - args
+                None
+            
+            - raise
+                if not enough depth exists, it'll raise Error. automatically re-generate omit-depth or other options, but it make side-effect that is unexpected.
+
+            - Return 
+                    DirectoryEnvironment : self instance
+        """
+        base_path = self.__get_base_path() #working dir & target_path 
         
 
-    def pre_setup():
+
+
+        
+        #extract directory list. NOT FILES NAME LIST!!
+        if not self.copy_detected : 
+            self.raw_data_pathes = self.__get_subdirectory(base_path)
+        else : 
+            self.raw_data_patches = self.copy_option_from_reference.raw_data_patches
+
+
+
+        #TODO post processing may needed.
+        
+
+        
+
+            
+
+        
+        return self
+
+
+
+    def __get_base_path(self):
+        return os.path.join(self.working_directory, self.target_path)
+
+
+    def __get_subdirectory(self, parent_path, depth = 0): 
+        """
+            recursive
+            path is list of string
+        """
+        if not os.path.isdir(parent_path) or self.search_depth == depth :
+            if depth == 0 : 
+                return [""]
+            else : 
+                return [os.path.basename(parent_path)]
+        
+        
+        path_list = []
+        for child_path in os.listdir(parent_path):
+            results = self.__get_subdirectory(os.path.join(parent_path, child_path), depth+1 )
+            
+            for result in results : 
+                if depth == 0 :
+                    path_list.append(result)
+                else : 
+                    path_list.append([parent_path].extend(result))
+        
+        return path_list
+
+
+
+            
+
+
+    def __omit_path_mask(self, path):
+        """
+            Method __omit_path : 
+                omit path base on Variable self.omit_desired_path
+                -args : 
+                    path : path calculated at __get_subdirectory. ex. ["C", "test", "path"] -> ["C", "path"]
+                -return omited path_list 
+
+        """ 
+        path_str = ""       
+        for idx in range(len(path)) :
+            if idx in self.omit_desired_depth : 
+                continue
+            path_str += path[idx]
+        return path_str
+
+
+            
+
+        
     
+    
+
+
     def __iter__(self):
         return self
+
     def __next__(self):
+        pass
         
 
     
--- typechecker.py
+++ typechecker.py
@@ -1,10 +1,83 @@
 
 
+class WeakTypeChecker(object) : 
+    """
+        Class WeakTypeChecker
+        WeakTypeChecker check primary type and reference type. but it's not support inside of reference.
+        
+    """
+    
+    def __init__(self) : 
+        """
+            Constructor __init__ 
+            _____________________
+            - Args 
+                None 
+            - Return
+                None
 
-class StrongTypeChecker(object) : 
+        """
+        self.__allowed_data_type = dict()
+        self.__num_key = 0
+
+
+
+    def _check_type(self, data): 
+        """
+            FUNCTION _check_type
+            ____________________
+            -args 
+                data : type or object
+            - return 
+                type
+        """
+        datatype=type(data)
+
+        if datatype is type : 
+            return datatype
+        return datatype     
+
+    def add_allowed_type(self, example_data):
+        """
+            Function add_allowed_type
+            _________________________
+            - Args 
+                example_data : example data structure that want to allowed.
+            - Return
+                (Int) key : return Key value.
+
+        """
+        #TODO check overlapped type.
+        def alloc_key() :
+            self.__num_key += 1 
+            return self.__num_key
+        
+
+        datatype = self._check_type(example_data)
+        key = alloc_key()
+        self.__allowed_data_type[key] = datatype 
+
+        return self
+    
+    def add_allowed_types(self, *example_data):
+        for data in example_data : 
+            self.add_allowed_type(data)
+        return self
+        
+    def __is_allowed_type(self, data):
+        
+        return self._check_type(data) in self.__allowed_data_type.values()
+    
+    def __call__(self, data) : 
+        return self.__is_allowed_type(data)
+
+
+class StrongTypeChecker(WeakTypeChecker) : 
     """
         Class StrongTypeChecker
-        
+        StrongTypeChecker check primary type and also check inside of refrence types. like dict, list, tuple, set
+        since StrongTypeChecker check inside of reference, type that inside of refrence must be same type.
+        it's not allowed in StrongTypeChecker. example) [[], 1, 2] -> list(list(), Integer, Integer) X
         
     """
     __container_type = [dict, list, tuple, set]
@@ -23,7 +96,7 @@ class StrongTypeChecker(object) :
 
 
 
-    def _check_type(self, inputs): 
+    def _check_type(self, data): 
         """
             FUNCTION _check_type
             ____________________
@@ -32,12 +105,41 @@ class StrongTypeChecker(object) :
             - return 
                 type
         """
-        datatype=type(inputs)
+        def recursive_container_checker(data_container):
+            dictionary_flag = False
+            
+            if not type(data_container) in StrongTypeChecker.__container_type : 
+                return type(data_container)
 
-        if datatype is type : 
-            datatype = inputs
-        else : 
-            pass
+            if type(data_container) == dict:
+                dictionary_flag = True
+            
+            prev_data_type =[ None ]
+            for data in data_container:
+                if dictionary_flag == True :
+                    key_type = recursive_container_checker(key)
+                    val_type = recursive_container_checker(data_container[key])
+                    data_type = [key_type, val_type]
+                else : 
+                    data_type=[ recursive_container_checker(data) ]
+                
+                if prev_data_type == None : 
+                    prev_data_type = data_type
+                    continue
+                
+                if prev_data_type == data_type :
+                    prev_data_type = data_type
+                else :
+                    raise TypeError("Type is Not Same.")
+                print("w",prev_data_type)
+                return prev_data_type
+                
+        
+        datatype = super(StrongTypeChecker, self)._check_type(data)
+
+        if datatype in StrongTypeChecker.__container_type : 
+            datatype = recursive_container_checker(data)
+
         return datatype     
 
     def add_allowed_type(self, example_data):
