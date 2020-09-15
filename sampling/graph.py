1f9fb6a5bf3f0d0dc41236f7c59e9d0f5b4ac7db
ROH <rohjihyun95@gmail.com>
2020-09-15 16:56:21 +0900

    ,,

---
+++ sampling/graph.py
@@ -0,0 +1,29 @@
+import sklearn.metrics
+import sklearn.neighbors
+import matplotlib.pyplot as plt
+import scipy.sparse
+import scipy.sparse.linalg
+import scipy.spatial.distance
+import numpy as np
+
+
+def laplacian(W, normalized=True):
+    """Return the Laplacian of the weigth matrix."""
+
+    # Degree matrix.
+    d = W.sum(axis=0)
+
+    # Laplacian matrix.
+    if not normalized:
+        D = scipy.sparse.diags(d.A.squeeze(), 0)
+        L = D - W
+    else:
+        d += np.spacing(np.array(0, W.dtype))
+        d = 1 / np.sqrt(d)
+        D = scipy.sparse.diags(d.A.squeeze(), 0)
+        I = scipy.sparse.identity(d.size, dtype=W.dtype)
+        L = I - D * W * D
+
+    # assert np.abs(L - L.T).mean() < 1e-9
+    assert type(L) is scipy.sparse.csr.csr_matrix
+    return L
